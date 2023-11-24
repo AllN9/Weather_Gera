2c47cb2dfddfb71d77bddd5adee24f8cca46b72b
Alexey < >
2023-11-24 18:17:38 +0300

    Создан бот, который показывает температуру на улице

--- Weather.py
+++ Weather.py
@@ -1,14 +1,26 @@
-# подключаем библиотеку для работы с запросами
 import requests
-# указываем город
-city = 'Сочи'
-# формируем запрос
-url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-# отправляем запрос на сервер и сразу получаем результат
-weather_data = requests.get(url).json()
-# получаем данные о температуре и о том, как она ощущается
-temperature = round(weather_data['main']['temp'])
-temperature_feels = round(weather_data['main']['feels_like'])
-# выводим значения на экран
-print('Сейчас в городе', city, str(temperature), '°C')
-print('Ощущается как', str(temperature_feels), '°C')
+import telebot
+
+bot = telebot.TeleBot('6391115122:AAGbHlcSycIYWC7pdF2VmnDHW3ePPCbqLyo');
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Привет, Я подсказываю температуру на улице, напиши желаемый город!")
+    else:
+        city = message.text
+        url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+        weather_data = requests.get(url).json()
+        
+        if(weather_data['cod'] == '404'):
+            bot.send_message(message.from_user.id, 'Такого города нет, напишите другой!')
+        else:
+            temperature = round(weather_data['main']['temp'])
+            message_temperature = 'Сейчас в городе' + ' ' + city + ' ' + str(temperature) + ' ' + '°C'
+            bot.send_message(message.from_user.id, message_temperature)
+
+            temperature_feels = round(weather_data['main']['feels_like'])
+            message_temperature_feels = 'Ощущается как' + ' ' + str(temperature_feels) + ' ' + '°C'
+            bot.send_message(message.from_user.id, message_temperature_feels)
+
+bot.polling(none_stop=True, interval=0)
